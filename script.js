223cb3e9a34d5610bfc7ce67fc169c51e580139b
shivraj khating <67963572+shivrajkhating@users.noreply.github.com>
2026-03-05 23:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const qtyInputs = document.querySelectorAll(".qty");
+const price = 120;
+
+function updateTotal(){
+
+let subtotal = 0;
+
+qtyInputs.forEach(input => {
+    subtotal += input.value * price;
+});
+
+document.getElementById("subtotal").innerText = subtotal;
+document.getElementById("total").innerText = subtotal;
+
+}
+
+qtyInputs.forEach(input=>{
+input.addEventListener("change", updateTotal);
+});
